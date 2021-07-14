830fdbb71cfe91aeb5383b1a9a9c74f247a5058e
mrbrazz <mrbrazz@yandex.ru>
2021-07-14 08:29:04 +0300

    Initial Commit

---
+++ UshakovSV_HW2.3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UshakovSV_HW2.3.1
+//
+//  Created by Сергей Ушаков on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
