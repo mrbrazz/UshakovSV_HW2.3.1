03be71c38f2b14d20c303de4b071acc1e13a93a8
mrbrazz <mrbrazz@yandex.ru>
2021-07-19 12:22:17 +0300

    add foot and gradientColor

--- UshakovSV_HW2.3.1/ViewControllers/WelcomeViewController.swift
+++ UshakovSV_HW2.3.1/ViewControllers/WelcomeViewController.swift
@@ -11,6 +11,9 @@ class WelcomeViewController: UIViewController {
     
     var login = ""
     
+    let firstColor = UIColor(red: 40/255, green: 50/255, blue: 255/255, alpha: 1)
+    let secondColor = UIColor(red: 220/255, green: 50/255, blue: 140/255, alpha: 1)
+    
     // MARK: - IB Outlet
     @IBOutlet weak var welcomeLable: UILabel!
     @IBOutlet weak var logoutButton: UIButton!
@@ -21,7 +24,20 @@ class WelcomeViewController: UIViewController {
     // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: firstColor, bottomColor: secondColor)
         welcomeLable.text = self.login
         welcomeLable.text = "Привет, \(login)! "
+    }
+}
+
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
